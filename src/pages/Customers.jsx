b2fccd52246efb69baaa91771353efeb9ae40a91
vikehieto <vikehietoyhoshii@gmail.com>
2022-06-14 09:00:51 +0530

    Employees, Orders, and Customers pages

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Sort,
+  Edit,
+  Toolbar,
+  Filter,
+  Search,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category={"Page"} title="Customers" />
+      <GridComponent
+        id="datacomp"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Search", "Delete"]}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Page, Search, Toolbar, Selection, Edit, Sort, Filter]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Header } from "../components";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Edit,
+  Inject,
+  Search,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
 
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category={"Page"} title="Employees"></Header>
+      <GridComponent
+        id="datacomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
